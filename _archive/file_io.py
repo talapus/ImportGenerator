40df22016614bdba7087618e32142202610cb2fc
Evan Hamlin <evan.hamlin1@gmail.com>
2015-01-12 17:14:02 -0800

    buncha ruffians

---
+++ _archive/file_io.py
@@ -0,0 +1,10 @@
+my_list = [i**2 for i in range(1,11)]
+# generates a list of squares from 1-11
+
+f = open("output.txt", "w")
+
+for item in my_list:
+    f.write(str(item) + "\n")
+
+f.close()
+print "File generated: output.txt"
