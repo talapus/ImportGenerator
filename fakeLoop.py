c4ceea49155feccd42d0dd872fb369bac884bfdb
Evan Hamlin <evan.hamlin1@gmail.com>
2015-01-28 10:08:22 -0800

    fakeLoop updates

--- fakeLoop.py
+++ fakeLoop.py
@@ -6,12 +6,12 @@ fake = Faker()
 
 
 def main():
+    ''' Get user input and print out the requested number of loops '''
     if len(sys.argv) <= 1:
         print "How many?"
     else:
         for i in range(0, int(sys.argv[1])):
             print (str(i) + " " + fake.name_male())
-
 
 # main
 
