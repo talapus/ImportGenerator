8caac14388e692960b017a6635de2cb01ed56f8a
Evan Hamlin <evan.hamlin1@gmail.com>
2015-01-28 20:40:53 -0800

    importGen update - working now

--- importGen.py
+++ importGen.py
@@ -7,10 +7,13 @@ from faker import Faker
 def main():
 
     if len(sys.argv) <= 1:
-        print "Please indicate the number of invitees"
+        print "How many?"
     else:
         gen = Faker()
-        print "Unique ID,First Name,Last Name,Email,Registration Code,Show on Attendee List,Company Name,Job Title,Brief Bio,Work Phone,Mobile Phone,Other Phone,Other Phone Type,Address,Address 2,City,State,Zipcode,Country,Website,Website 2,Linkedin URL,Twitter URL,Facebook URL,Group List (comma-separated)"
+        print "Unique ID,First Name,Last Name,Email,Registration Code,Show on Attendee List,Company Name,\
+                Job Title,Brief Bio,Work Phone,Mobile Phone,Other Phone,Other Phone Type,Address,Address 2,\
+                City,State,Zipcode,Country,Website,Website 2,Linkedin URL,Twitter URL,Facebook URL,\
+                Group List (comma-separated)"
         for i in range(0, int(sys.argv[1])):
             print "A-" + str(i+1) + ", " + gen.first_name() + ", " + gen.last_name() + ", " + gen.email() + ", " + " " + \
                 ", " + "FALSE" + ", " + gen.word() + ", " + gen.slug() + ", " + gen.bs() + ", " + \
