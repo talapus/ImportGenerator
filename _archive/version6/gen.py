1bba3c85492f8f3c93fbc03f46e4e835c8958b33
talapus <evan.hamlin1@gmail.com>
2015-02-02 22:35:37 -0800

    removed the cruft. Kept first and most recent versions for later reflection

--- _archive/version6/gen.py
+++ _archive/version6/gen.py
@@ -17,18 +17,18 @@ def main():
             os.makedirs(dName)
         with open("{0}/{1}".format(dName,filename), "wb") as output:
             csvWriter = csv.writer(output)
-            zlist = ["Unique ID", "First Name", "Last Name", "Email", "Registration Code",\
-                "Show on Attendee List", "Company Name", "Job Title", "Brief Bio",\
-                "Work Phone", "Mobile Phone", "Other Phone", "Other Phone Type",\
-                "Address", "Address 2", "City", "State", "Zipcode", "Country",\
-                "Website", "Website 2", "Linkedin URL", "Twitter URL", "Facebook URL",\
+            zlist = ["Unique ID", "First Name", "Last Name", "Email", "Registration Code",
+                "Show on Attendee List", "Company Name", "Job Title", "Brief Bio",
+                "Work Phone", "Mobile Phone", "Other Phone", "Other Phone Type",
+                "Address", "Address 2", "City", "State", "Zipcode", "Country",
+                "Website", "Website 2", "Linkedin URL", "Twitter URL", "Facebook URL",
                 "Group List (comma-separated)"]
             csvWriter.writerow(zlist)
             for i in range(0,int(sys.argv[1])):
-                ylist = ["A-{0}".format(i), fake.first_name(), fake.last_name(),\
-                    fake.email(), " ", "FALSE", fake.word(), fake.slug(), fake.bs(),\
+                ylist = ["A-{0}".format(i), fake.first_name(), fake.last_name(),
+                    fake.email(), " ", "FALSE", fake.word(), fake.slug(), fake.bs(),
                     fake.phone_number(), " ", " ", " ", " ", " ", fake.city(),
-                    fake.state(), fake.postcode(), fake.country(), fake.url(),\
+                    fake.state(), fake.postcode(), fake.country(), fake.url(),
                     fake.url(), " ", " ", " ", " ", ]
                 csvWriter.writerow(ylist)
             print "{0}/{1}".format(dName, filename)
