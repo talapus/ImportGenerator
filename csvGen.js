816eb198f9dc0c415c410682c7093d85d872e1e1
Evan Hamlin <evan.hamlin1@gmail.com>
2015-02-17 10:24:38 -0800

    new and improved csvGen. looping record export into a file works. Now need to flesh out what it exports again

--- csvGen.js
+++ csvGen.js
@@ -1,54 +1,21 @@
 #!/usr/bin/env node
 
-/*
- * A command line script to produce mocked data output for the
- * CrowdCompass CSV input interfaces.
- *
- * Usage: gsvGen [#]
-*/
-
 var fake = require('faker');
 var fs = require('fs');
+//var localeKey = ["en_US", "en_AU", "en_UK"];
+var localeKey = "en_US";  // lets just go with English for now and make it functional
 
-var URL = function() { return("http://www." + fake.internet.domainName() + "/" + fake.lorem.words(1)); };
-var Company = function() { return(fake.company.bsAdjective() + " " + fake.company.bsNoun())};
-var Job = function() { return(fake.company.catchPhraseAdjective() + " " + fake.company.catchPhraseNoun() + " Specialist") };
-
-var locales = ['de', 'de_AT','de_CH','en','en_AU','en_BORK','en_CA','en_GB','en_IND',
-  'en_US','en_au_ocker','es','fa','fr','it','ja','ko','nb_NO','nep','nl','pl','pt_BR',
-  'ru','sk','sv','vi','zh_CN'];
-
-if (!process.argv[2]) {
+if (!process.argv[2] || isNaN(+process.argv[2])) {
   console.log("Usage: csvGen #");
-} else {
-  input = +process.argv[2];
-  console.log("Unique ID, First Name, Last Name, Email, Registration Code, Show on Attendee List, Company Name, Job Title, Brief Bio, Work Phone, Mobile Phone, Other Phone, Other Phone Type, Address, Address 2, City, State, Zipcode, Country, Website, Website 2, Linkedin URL, Twitter URL, Facebook URL, Group List (comma-separated)");
-  for (var i=0; i < input; i++) {
-      for (i=0; i < locales.length; i++) {
-        fake.locale = locales[i];
-        var stream = fs.createWriteStream(locales[i] + "_import.csv");
-        stream.once('open', function(fd) {
-          stream.write("A-" + (i+1) + ", " + fake.name.firstName() + ", " + fake.name.lastName()
-               + ", " + fake.internet.email() + ", " + ", " + "FALSE, " + Company()
-               + ", " + Job() + ", " + ", " + fake.phone.phoneNumber() + ", " + ", "
-               + ", " + ", " + fake.address.streetAddress() + ", " + ", " + fake.address.city()
-               + ", " + fake.address.state() + ", " + fake.address.zipCode()
-               + ", " + fake.address.country() + ", " + URL() + ", " + URL()
-               + ", " + ", " + ", " + ", ");
-          stream.end();
-      }
-   }
+} else { // only write a file if the user indicates how many records
+  userInput = +process.argv[2];
+  var stream = fs.createWriteStream("en_US.csv");
+  stream.once('open', function(fd) {
+    stream.write("Record, First Name, Last Name, Email \n");
+    for (i=0; i<userInput; i++) {
+      stream.write(fake.name.firstName() + ", " + fake.name.lastName() + ", " + fake.internet.email() + "\n");
+    }
+    stream.end();
+    console.log(localeKey + ".csv created");
+  });
 }
-
-/*
-
-var stream = fs.createWriteStream("my_file.txt");
-stream.once('open', function(fd) {
-  stream.write("My first row\n");
-  stream.write("My second row\n");
-  stream.end();
-});
-
-console.log("File output as my_file.txt");
-
-*/
