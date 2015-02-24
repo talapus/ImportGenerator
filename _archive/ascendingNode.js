8e48ffbee65ee956d668cad661f0f266e6c73f33
Evan Hamlin <evan.hamlin1@gmail.com>
2015-02-23 19:41:58 -0800

    ascendingNode added to _archive

---
+++ _archive/ascendingNode.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+
+console.log(String.fromCharCode(0x0000,0x260a) + "\tU+260a" + "\tAscending Node");
+console.log(String.fromCharCode(0x0000,0x260b) + "\tU+260b" + "\tDescending Node");
