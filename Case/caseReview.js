8baa45b48458d0c26ec229a080b7bc5009ffc0ce
Evan Hamlin <evan.hamlin1@gmail.com>
2015-02-23 19:42:39 -0800

    making caseReview functional

--- Case/caseReview.js
+++ Case/caseReview.js
@@ -1,18 +1,27 @@
+#!/usr/bin/env node
+
 var getReview = function (movie) {
     switch (movie) {
         case 'Toy Story 2':
         return("Great story. Mean prospector.");
         break;
-        
+
         case 'Finding Nemo':
         return("Cool animation, and funny turtles.");
         break;
-    
+
         case 'The Lion King':
         return("Great songs.");
         break;
-    
+
         default:
         return("I don't know!");
     }
 };
+
+if (!process.argv[2]) {
+  console.log(String.fromCharCode(0x0000,0x260a));
+} else {
+  userInput = process.argv[2];
+  getReview(userInput);
+}
