7025807629d07cf90ce46cc6280631c2f478737e
Evan Hamlin <evan.hamlin1@gmail.com>
2015-02-23 19:42:18 -0800

    ocrSymbols

---
+++ Case/ocrSymbols.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+console.log(String.fromCharCode(0x0000,0x2440) + "\tU+2440" + "\tOCR hook");
+console.log(String.fromCharCode(0x0000,0x2441) + "\tU+2441" + "\tOCR chair");
+console.log(String.fromCharCode(0x0000,0x2442) + "\tU+2442" + "\tOCR fork");
+console.log(String.fromCharCode(0x0000,0x2443) + "\tU+2443" + "\tOCR inverted fork");
+console.log(String.fromCharCode(0x0000,0x2444) + "\tU+2444" + "\tOCR belt buckle");
+console.log(String.fromCharCode(0x0000,0x2445) + "\tU+2445" + "\tOCR bow tie");
+console.log(String.fromCharCode(0x0000,0x2446) + "\tU+2446" + "\tOCR branch bank");
+console.log(String.fromCharCode(0x0000,0x2447) + "\tU+2447" + "\tOCR amount of check");
+console.log(String.fromCharCode(0x0000,0x2448) + "\tU+2448" + "\tOCR dash");
+console.log(String.fromCharCode(0x0000,0x2449) + "\tU+2449" + "\tOCR customer account number");
