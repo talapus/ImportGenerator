c0adcfe2705e4f1068dd4e79051a9d7e08c8f278
Evan Hamlin <evan.hamlin1@gmail.com>
2015-03-23 17:23:48 -0700

    BigBio

--- BigBio.js
+++ BigBio.js
@@ -1,16 +1,29 @@
 #!/usr/bin/env node
 
+var fake = require('faker')
+
+/* old experiments
+
 //var fake = require('faker');
 
 var BigBio = function() {
   var whatever = "test" //fake.company.bsAdjective();
 
-  /*
   for (var i=0; i<0; i++) {
     whatever = whatever + " " + fake.company.bsAdjective();
-  }*/
+  }
   return(whatever);
   //return('test');
-}
+} */
 
-console.log(BigBio());
+// this returns only one name
+//var BigBio = function() {for (var i=0; i<30; i++) {return(fake.name.firstName())}}
+// console.log(BigBio());
+
+/* this works, but I don't want to just spew onto the console
+var BigBio = function() {for (var i=0; i<30; i++) {console.log(fake.name.firstName())}}
+BigBio();
+*/
+
+var names = [];
+var BigBio = function() {for (var i=0; i<30; i++) {console.log(fake.name.firstName())}}
