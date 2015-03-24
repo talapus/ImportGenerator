86b4804cf1494bf76648b22e49073b250b6e2682
Evan Hamlin <evan.hamlin1@gmail.com>
2015-03-23 17:15:48 -0700

    BigBio

---
+++ BigBio.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+//var fake = require('faker');
+
+var BigBio = function() {
+  var whatever = "test" //fake.company.bsAdjective();
+
+  /*
+  for (var i=0; i<0; i++) {
+    whatever = whatever + " " + fake.company.bsAdjective();
+  }*/
+  return(whatever);
+  //return('test');
+}
+
+console.log(BigBio());
