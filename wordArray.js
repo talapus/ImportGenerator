e80acf5ef2d4badc0fbbb8b35553ded591837a20
Evan Hamlin <evan.hamlin1@gmail.com>
2015-04-06 13:09:02 -0700

    wordArray

--- wordArray.js
+++ wordArray.js
@@ -4,12 +4,11 @@ var fake = require('faker');
 
 var bigBio = function(input) {
   wordArray = [];
-  console.log("wordArray before loop: " + wordArray);
 
   for (var i=0; i<input; i++) {
     wordArray[i] = fake.hacker.noun();
   };
-  console.log("wordArray after loop: " + wordArray);
+  console.log(wordArray + " ");
 }
 
 bigBio(3);
